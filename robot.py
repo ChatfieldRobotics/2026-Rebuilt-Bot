1bf76c5961ccf6da048e632f8e0bb91e93682c06
Wagner Henry <2123654@jeffcoschools.us>
2026-01-28 11:02:06 -0700

    Fixed a small issue in robot.py

--- robot.py
+++ robot.py
@@ -1,5 +1,6 @@
 from robot_container import RobotContainer
 from commands2 import CommandScheduler
+from wpilib import DriverStation
 
 import wpilib
 
@@ -11,9 +12,9 @@ class Robot(wpilib.TimedRobot):
         data = wpilib.DriverStation.getGameSpecificMessage()
         if data:
             if data == "R" and (DriverStation.getAlliance() | DriverStation.Alliance.kBlue) == DriverStation.Alliance.kRed:
-                robot_container.active_rumble()
+                self.robot_container.active_rumble()
             elif data =="B" and (DriverStation.getAlliance() | DriverStation.Alliance.kRed) == DriverStation.Alliance.kBlue:
-                robot_container.active_rumble()
+                self.robot_container.active_rumble()
         
         CommandScheduler.getInstance().run()
 
